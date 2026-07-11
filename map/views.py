88dc49bc5a239e5b9dc9ba8595722d4c237d26bc
Vladislav <yudin.labor@yandex.ru>
2026-07-11 22:54:23 +0300

    Кластеризация Лувена выполнена.

--- map/views.py
+++ map/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import RouteForm
-from .utils.graph_utils import create_road_graph, find_shortest_path, find_nearest_node
+from .utils.graph_utils import create_road_graph, find_shortest_path, find_nearest_node, build_combined_graph, detect_communities
 from .serializers import NodeSerializer
+from .models import Node
 
 ROADS_GEOJSON = "roads_drive_spb.geojson"
 
@@ -11,41 +12,70 @@ def map_view(request):
     path_coordinates = []
     start_geometry = None
     end_geometry = None
+    communities = None
+    nodes_data = []
 
     if request.method == 'POST':
         form = RouteForm(request.POST)
         if form.is_valid():
-            start_node = form.cleaned_data['start_node']
-            end_node = form.cleaned_data['end_node']
+            if 'find_route' in request.POST:
+                start_node = form.cleaned_data['start_node']
+                end_node = form.cleaned_data['end_node']
 
-            # Получаем координаты начальной и конечной точек
-            start_coords = (start_node.geom.centroid.x, start_node.geom.centroid.y)
-            end_coords = (end_node.geom.centroid.x, end_node.geom.centroid.y)
+                # Получаем координаты начальной и конечной точек
+                start_coords = (start_node.geom.centroid.x, start_node.geom.centroid.y)
+                end_coords = (end_node.geom.centroid.x, end_node.geom.centroid.y)
 
-            # Создаём граф дорог
-            graph = create_road_graph(ROADS_GEOJSON)
+                # Создаём граф дорог
+                graph = create_road_graph(ROADS_GEOJSON)
 
-            # Находим ближайшие вершины графа
-            start_nearest = find_nearest_node(graph, start_coords)
-            end_nearest = find_nearest_node(graph, end_coords)
+                # Находим ближайшие вершины графа
+                start_nearest = find_nearest_node(graph, start_coords)
+                end_nearest = find_nearest_node(graph, end_coords)
 
-            # Находим кратчайший путь
-            path = find_shortest_path(graph, start_nearest, end_nearest)
+                # Находим кратчайший путь
+                path = find_shortest_path(graph, start_nearest, end_nearest)
 
-            # Получаем координаты для пути
-            if path:
-                path_coordinates = [[y, x] for x, y in path]  # Преобразуем в [широта, долгота]
-                print("Найден путь:", path)  # Отладочная информация
-                print("Координаты пути:", path_coordinates)  # Отладочная информация
+                # Получаем координаты для пути
+                if path:
+                    path_coordinates = [[y, x] for x, y in path]  # Преобразуем в [широта, долгота]
 
-            # Используем NodeSerializer для получения GeoJSON
-            print(start_node)
-            print(end_node)
-            # Преобразуем координаты в GeoJSON-полигон
-            start_geometry = NodeSerializer(start_node).data['geometry']
-            end_geometry = NodeSerializer(end_node).data['geometry']
-            print(start_geometry)
-            print(end_geometry)
+                # Используем NodeSerializer для получения GeoJSON
+
+                start_geometry = NodeSerializer(start_node).data['geometry']
+                end_geometry = NodeSerializer(end_node).data['geometry']
+
+
+            elif 'detect_communities' in request.POST:  # Если нажата кнопка "Кластеризация Лувена"
+
+                # Логика для кластеризации
+
+                graph = build_combined_graph()
+
+                communities = detect_communities(graph)
+
+                # Получаем данные о зданиях (узлах) для визуализации
+
+                nodes_data = []
+
+                for node in Node.objects.all():
+
+                    # Преобразуем координаты из кортежей в списки
+
+                    geom_coords = [[y, x] for x, y in node.geom.coords[0]]
+
+                    nodes_data.append({
+
+                        'id': node.id,
+
+                        'name': node.name,
+
+                        'geom': geom_coords,  # Координаты полигона здания (уже в формате [[lat, lon], ...])
+
+                        'community': communities.get(node.id, -1)  # Номер сообщества
+
+                    })
+
     else:
         form = RouteForm()
 
@@ -54,7 +84,10 @@ def map_view(request):
         'path': path,
         'path_coordinates': path_coordinates,
         'start_geometry': start_geometry,
-        'end_geometry': end_geometry})
+        'end_geometry': end_geometry,
+        'communities': communities,
+        'nodes_data': nodes_data
+    })
 
 '''
 from django.shortcuts import render
