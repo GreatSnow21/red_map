3d9b2c42d176159a5761abc65e4561cfbe50e33d
Vladislav <yudin.labor@yandex.ru>
2026-07-11 22:54:24 +0300

    Add ALLOWED_HOSTS

--- red_map/settings.py
+++ red_map/settings.py
@@ -16,6 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 # Application definition
 
 INSTALLED_APPS = [
